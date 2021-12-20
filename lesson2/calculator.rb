fc06b83d200207da6d0d87f7ee127335f1d94cd5
Travis Linville <travislinville86@gmail.com>
2021-12-19 23:49:27 -0500

    First commit

---
+++ lesson2/calculator.rb
@@ -0,0 +1,27 @@
+# Ask user for 2 numbers
+# Ask user for operation to perform
+# Perform operation on 2 numbers
+# Output the results
+
+Kernel.puts("Welcome to Calculator")
+
+Kernel.puts("What's the first number?")
+number1 = Kernel.gets().chomp()
+
+Kernel.puts("What's the second number?")
+number2 = Kernel.gets().chomp()
+
+Kernel.puts("What operation would you like to perform? 1) add, 2) subtract, 3) multiply, 4) divide")
+operator = Kernel.gets().chomp()
+
+if operator == '1'
+  result = number1.to_i() + number2.to_i()
+elsif operator == '2'
+  result = number1.to_i() - number2.to_i()
+elsif operator == '3'
+  result = number1.to_i() * number2.to_i()
+else
+  result = number1.to_f() / number2.to_f()
+end
+
+Kernel.puts("The result is #{result}.")
---
+++ lesson2/pseudocode.rb
@@ -0,0 +1,52 @@
+# 1) a method that returns the sum of 2 integers
+# Casual
+# Given 2 integers
+# Add integer1 + integer2
+# return sum
+
+# Formal
+# START
+# # Given 2 integers
+# PRINT integer1 + integer2
+# END
+
+# 2) a method that takes an array of strings and returns a string that is
+# all those strings concatenated together
+# Casual
+# Given an array of strings
+# Create a new variable concatenated_string as an empty string
+# Iterate through each string in the array
+# - add each string to the concatenated_string variable
+# After finished iterating through the array, return concatenated_string
+
+# Formal
+# START
+# # Given an array of strings called "strings"
+# SET variable concatenated_string = ''
+# READ each index string in strings
+#   concatenated_string += string
+# PRINT concatenated_string
+# END
+
+
+# 3) a method that takes an array of integers and returns a new array with
+# every other element
+# Casual
+# Given an array of integers
+# Iterate through elements of the array
+# - select the first element of the array (index 0)
+# - select the even numbered elements of the array
+
+
+# Formal
+# START
+# # Given an array of integers int_array
+# SET new_arr = []
+# SET first integer in int_array to new_arr
+# READ each integer in int_array
+#   IF the index of integer is even
+#     SET integer to new_arr
+#   ELSE do nothing
+# PRINT new_arr
+
+# END
